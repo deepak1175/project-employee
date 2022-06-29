38b8e71f1f6be39010baea95f3b6e2f29a80733f
deepak1175 <74853754+deepak1175@users.noreply.github.com>
2022-06-29 21:06:58 +0530

    Add files via upload

---
+++ routes/employee.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const upload = require('../middleware/upload.js')
+ const EmployeeController = require('../controllers/EmployeeController.js');
+const authenticate = require('../middleware/authenticate');
+ router.get('/', authenticate, EmployeeController.index);
+
+ router.get('/show/:id', EmployeeController.show);
+
+ router.post('/store',  authenticate, upload.single('avatar'), EmployeeController.store);
+ router.put('/update/:id', EmployeeController.update);
+ router.delete('/delete/:id', EmployeeController.destroy);
+
+module.exports = router;
+
+
